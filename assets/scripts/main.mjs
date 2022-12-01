da96d45bb978549e6ec48f061ebf69485c2eb7dc
Nicolas G <48217175+nicolasgodineau@users.noreply.github.com>
2022-12-01 17:08:25 -0500

    Simplification du JS, ajout de cartes timbres dans la fiche

--- assets/scripts/main.mjs
+++ assets/scripts/main.mjs
@@ -3,15 +3,8 @@ import { CatalogueTimbres } from "../data/data.js";
 const catalogue = document.querySelector("[data='catalogue']");
 const cartes = document.querySelectorAll(".carte");
 
-console.log(document);
-if (document.title.toLocaleLowerCase() == "catalogue") {
-    // Permet d'appeler la fonction si le titre de la page est catalogue
-    remplissageCatalogue(); // appel de la fonction
-} else if (document.title.toLocaleLowerCase() == "accueil") {
-    // Permet d'appeler la fonction si le titre de la page est accueil
-    remplissageCartesIndex(); // appel de la fonction
-}
-function remplissageCatalogue() {
+remplissageInformationTimbre();
+function remplissageInformationTimbre() {
     cartes.forEach((element, index) => {
         // création d'un dataset pour identifier les timbres
         let datasetTimbre = element.dataset.timbre;
@@ -30,79 +23,6 @@ function remplissageCatalogue() {
         nomTimbre.textContent = CatalogueTimbres[index].titre;
 
         let infoImages = element.querySelector("[data='image']");
-        console.log("infoImages:", infoImages);
-        infoImages.src = CatalogueTimbres[index].image;
-
-        let timer = setInterval(function () {
-            // date d'échéance
-            let dateFin = new Date(
-                CatalogueTimbres[index].finEnchere
-            ).getTime();
-            // date du jours
-            const dateMaintenant = new Date().getTime();
-
-            // calcule l'interval entre la date du jour et la date de fin
-            const distance = dateFin - dateMaintenant;
-
-            // calcul des jours, heures, minutes et secondes
-            const jours = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const heures = Math.floor(
-                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-            );
-            var minutes = Math.floor(
-                (distance % (1000 * 60 * 60)) / (1000 * 60)
-            );
-            var secondes = Math.floor((distance % (1000 * 60)) / 1000);
-
-            // Display the result in the element with id="demo"
-            element.querySelector(".heure h2").innerHTML =
-                jours + "j " + heures + "h " + minutes + "m " + secondes + "s ";
-
-            // si le timer est terminer on affiche un message
-            if (distance < 0) {
-                element.querySelector(".heure h2").innerHTML = "Terminer";
-            }
-        }, 1000); // acctualisation toutes les secondes
-
-        // modification du svg like
-        let ajoutLike = element.querySelector(".like .icon_like");
-
-        ajoutLike.addEventListener("click", function () {
-            let coeur = element.querySelector(".like .icon_like");
-            // changement de la couleur du coeur
-            coeur.classList.toggle("liker");
-
-            // si il y a changement de class (coeur rouge) alors +1 pour les likes, si le coeur rouge devient noir alors like-1
-            if (coeur.classList.contains("liker")) {
-                CatalogueTimbres[index].like++;
-            } else {
-                CatalogueTimbres[index].like--;
-            }
-            infoLike.textContent = CatalogueTimbres[index].like;
-        });
-    });
-}
-
-function remplissageCartesIndex() {
-    cartes.forEach((element, index) => {
-        // création d'un dataset pour identifier les timbres
-        let datasetTimbre = element.dataset.timbre;
-        datasetTimbre = CatalogueTimbres[index].timbre;
-
-        let infoPrix = element.querySelector("[data='prix']");
-        infoPrix.textContent = "$" + CatalogueTimbres[index].prix + ".00";
-
-        let infoLike = element.querySelector("[data='like']");
-        infoLike.textContent = CatalogueTimbres[index].like;
-
-        let infoEtat = element.querySelector("[data='etat']");
-        infoEtat.textContent = CatalogueTimbres[index].etat;
-
-        let nomTimbre = element.querySelector("[data='nomTimbre']");
-        nomTimbre.textContent = CatalogueTimbres[index].titre;
-
-        let infoImages = element.querySelector("[data='image']");
-        console.log("infoImages:", infoImages);
         infoImages.src = CatalogueTimbres[index].image;
 
         let timer = setInterval(function () {
