2dcd2edb9851c0651a2ac098def9aee68596e179
Nicolas G <48217175+nicolasgodineau@users.noreply.github.com>
2023-01-23 08:37:04 -0500

    Modification de la page compte ATTENTION modification back NON TESTER

--- controller/ControllerEnchere.php
+++ controller/ControllerEnchere.php
@@ -3,7 +3,8 @@ RequirePage::requireModel('Crud');
 RequirePage::requireModel('ModelMembre');
 RequirePage::requireModel('ModelPays');
 RequirePage::requireModel('ModelRole');
-RequirePage::requireModel('Modeltimbre');
+RequirePage::requireModel('ModelTimbre');
+RequirePage::requireModel('ModelEnchere');
 
 
 class ControllerEnchere
@@ -11,8 +12,7 @@ class ControllerEnchere
 
     public function create()
     {
-
-        //CheckSession::sessionAuth();
+        CheckSession::sessionAuth();
         // Pour avoir l'id du membre connecter via la session 
         // Alternative
         // $membre = new ModelMembre;
@@ -26,20 +26,21 @@ class ControllerEnchere
 
     public function store()
     {
-        echo '<pre>';
-        print_r($_SESSION["idMembre"]);
-        echo '</pre>';
-        echo "text enchère";
-        die();
-        $membre = new ModelMembre;
-        // Passe en paramètre l'id de la session du membre connecter
-        $selectMembre = $membre->selectId($_SESSION["idMembre"]);
-
-        $timbre = new ModelTimbre;
+        //$timbre = new ModelTimbre;
         //$timbreInsert = $timbre->insert($_POST);
+
+        $enchere = new ModelEnchere;
+        $enchereInsert = $enchere->insert($_POST);
         echo '<pre>';
-        print_r($timbre);
+        print_r($enchereInsert);
         echo '</pre>';
+        die();
+
+        // NOTE l'insert dans la table enchère ne marche pas
+        // Piste à chercher:
+        // Il faut peuetre faire l'insert du timbre dans sa table, puis récupere l'id du timbre, via lastinsert (chercher dans le code ou c'est)
+        // avec ce lastInsert, on peut créer un tableau ? ou faire un insert dans la table enchère de cet id du timbre, il faut aussi l'id du membre
+
 
         //twig::render("enchere-create.php",['membre' => $selectMembre]);
     }
--- view/membre-index.php
+++ view/membre-index.php
@@ -11,79 +11,28 @@
         <i class="fa-solid fa-arrow-right"></i>
         <em>Compte</em>
     </nav>
-    <main class="compte flex_row flex_align_center">
-        <section class="compte_profil flex_column">
-            <div class="compte_photo ">
-                <img src="assets/img/icons/user.svg" alt="Image" class="shadow">
-            </div>
-            <h4 class="text-center">Nicolas Godineau</h4>
-            <div class="compte_menu flex_column">
-                <details open>
-                    <summary>
-                        <h4>Créer une enchère</h4>
-                    </summary>
-                    <h6>Création d'une enchere</h6>
-                    <form class="flex_column" action="{{ path }}membre/update" method="post">
-                        <div class="info_perso flex_column">
-                            <input type="text" name="idMembre" value="{{membre.idMembre}}">
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-user"></i><input aria-label="nomTimbre" type="text"
-                                    name="nomTimbre" id="nomTimbre" placeholder="Nom du timbre" value="">
-                            </div>
-                        </div>
-                    </form>
-                </details>
-                <details>
-                    <summary>
-                        <i class="fa-solid fa-user"></i>
-                        <h4>Informations personnelles</h4>
-                    </summary>
-                    <form class="flex_column" action="{{ path }}membre/update" method="post">
-                        <div class="info_perso flex_column">
-                            <input type="hidden" name="Role_idRole" value=1>
-                            <input type="hidden" name="idMembre" value="{{membre.idMembre}}">
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-user"></i><input aria-label="nom" type="text" name="nom" id="nom"
-                                    placeholder="Nom" value="{{membre.nom}}" pattern="[A-Za-z]{1,15}"
-                                    title="Champ prénom invalide 15 lettres maximum exemple: john">
-                            </div>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-user"></i><input aria-label="prenom" type="text" name="prenom"
-                                    id="prenom" placeholder="Prénom" value="{{membre.prenom}}">
-                            </div>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-phone"></i><input aria-label="telephone" type="text"
-                                    name="telephone" id="telephone" placeholder="Téléphone"
-                                    value="{{membre.telephone}}">
-                            </div>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-house"></i><input aria-label="adresse" type="text" name="adresse"
-                                    id="adresse" placeholder="Adresse" value="{{membre.adresse}}">
-                            </div>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-house"></i>
-                                <select class="placeholder" aria-label="pays" type="text" name="pays" id="pays"
-                                    placeholder="Pays" required">
-                                    <option selected>Votre pays</option>
-                                    {% for pays in paysS %}
-                                    <option value="{{ pays.idPays }}" {% if pays.idPays == membre.Pays_idPays %}
-                                        selected {% endif %}>
-                                        {{pays.pays}}</option>
-
-                                    {% endfor %}
-                                </select>
-                            </div>
-                            <input class="call_to_action bleu" type="submit" value="Sauvegarder">
-                        </div>
-                    </form>
-                </details>
-                <details>
-                    <summary>
-                        <i class="fa-solid fa-user"></i>
-                        <h4>Date de naissance</h4>
-                    </summary>
+    <main class="compte formulaire_inscription flex_column">
+        <div class="compte_photo flex_column flex_justify_center flex_align_center">
+            <i class="fa-solid fa-user"></i>
+        </div>
+        <form class="flex_column" action="{{ path }}membre/update" method="post">
+            <details>
+                <summary>
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                    <h4>Informations personnelles</h4>
+                </summary>
+                <div class="info_perso flex_column">
+                    <input type="hidden" name="Role_idRole" value=1>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-user"></i><input aria-label="nom" type="text" name="nom" id="nom"
+                            placeholder="Nom" value="{{membre.nom}}" pattern="[A-Za-z]{1,15}"
+                            title="Champ prénom invalide 15 lettres maximum exemple: john">
+                    </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-user"></i><input aria-label="prenom" type="text" name="prenom" id="prenom"
+                            placeholder="Prénom" value="{{membre.prenom}}">
+                    </div>
                     <div class="info_date_naissance flex_column">
-
                         <div class="date_naissance flex_row">
                             <div class="flex_row flex_align_center">
                                 <i class="fa-solid fa-cake-candles"></i><input aria-label="jourNaissance" type="text"
@@ -99,33 +48,80 @@
                             </div>
                         </div>
                     </div>
-                </details>
-                <details>
-                    <summary>
-                        <i class="fa-solid fa-lock"></i>
-                        <h4>Mot de passe</h4>
-                    </summary>
-                    <form class="flex_column" action="" method="post">
-                        <div class="info_connexion flex_column">
-                            <h4>Changer votre mot de passe</h4>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-lock"></i><input aria-label="password" type="text" name="password"
-                                    id="password" placeholder="Ancien mot de passe" required>
-                            </div>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-lock"></i><input aria-label="password" type="text" name="password"
-                                    id="password" placeholder="Nouveau mot de passe" required>
-                            </div>
-                            <div class="flex_row flex_align_center">
-                                <i class="fa-solid fa-lock"></i><input aria-label="passwordConfirm" type="text"
-                                    name="passwordConfirm" id="passwordConfirm" placeholder="Confirmer le mot de passe"
-                                    required>
-                            </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-phone"></i><input aria-label="telephone" type="text" name="telephone"
+                            id="telephone" placeholder="Téléphone" value="{{membre.telephone}}">
+                    </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-house"></i><input aria-label="adresse" type="text" name="adresse"
+                            id="adresse" placeholder="Adresse" value="{{membre.adresse}}">
+                    </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-house"></i>
+                        <select class="placeholder" aria-label="pays" type="text" name="pays" id="pays"
+                            placeholder="Pays" required">
+                            <option selected>Votre pays</option>
+                            {% for pays in paysS %}
+                            <option value="{{pays.idPays}}">{{pays.pays}}</option>
+                            {% endfor %}
+                        </select>
+                    </div>
+            </details>
+            <details>
+                <summary>
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                    <h4>Informations de paiement</h4>
+                </summary>
+                <div class="info_perso flex_column">
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-credit-card"></i><input aria-label="carte_credit_numero" type="text"
+                            name="carte_credit_numero" id="carte_credit_numero" placeholder="Nom et prénom du titulaire"
+                            required>
+                    </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-credit-card"></i><input aria-label="carte_credit_numero" type="text"
+                            name="carte_credit_numero" id="carte_credit_numero"
+                            placeholder="Numéro de la carte de crédit" required>
+                    </div>
+                    <div class="date_naissance flex_row">
+                        <div class="flex_row flex_align_center">
+                            <i class="fa-solid fa-credit-card"></i><input aria-label="carte_credit_mois" type="text"
+                                name="carte_credit_mois" id="carte_credit_mois" placeholder="MM" required>
                         </div>
-                        <button class="call_to_action bleu" type="submit">Enregister les modifications</button>
-                    </form>
-                </details>
-            </div>
-        </section>
+                        <div class="flex_row flex_align_center">
+                            <i class="fa-solid fa-credit-card"></i><input aria-label="carte_credit_annee" type="text"
+                                name="carte_credit_annee" id="carte_credit_annee" placeholder="AA" required>
+                        </div>
+                        <div class="flex_row flex_align_center">
+                            <i class="fa-solid fa-credit-card"></i><input aria-label="carte_credit_cvc" type="text"
+                                name="carte_credit_cvc" id="carte_credit_cvc" placeholder="CVC" required>
+                        </div>
+                    </div>
+                </div>
+            </details>
+            <details>
+                <summary>
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                    <h4>Informations de connexion</h4>
+                </summary>
+                <div class="info_connexion flex_column">
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-envelope"></i><input aria-label="email" type="email" name="email"
+                            id="email" placeholder="Email" value="{{ membre.email }}" disabled>
+                    </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-lock"></i><input aria-label="password" type="text" name="password"
+                            id="password" placeholder="Nouveau mot de passe" pattern="[0-9]{6,9}"
+                            title="6 caractères minimum et uniquement des chiffres" required>
+                    </div>
+                    <div class="flex_row flex_align_center">
+                        <i class="fa-solid fa-lock"></i><input aria-label="passwordConfirm" type="text"
+                            name="passwordConfirm" id="passwordConfirm" placeholder="Confirmer le mot de passe"
+                            required>
+                    </div>
+                </div>
+            </details>
+            <input class="call_to_action bleu" type="submit" value="Enregister les modifications">
+        </form>
     </main>
     {{ include('footer.php') }}
